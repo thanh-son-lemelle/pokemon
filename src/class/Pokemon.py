7f6761578db1aa2727522eb914bfb97c1d241aaa
thanh-son-lemelle <thanh-son.lemelle@laplateforme.io>
2024-01-13 19:25:25 +0100

    update des methodes relatives au level up & de la croissance des stats

--- src/class/Pokemon.py
+++ src/class/Pokemon.py
@@ -2,15 +2,16 @@
 import json
 import pygame
 import sys
+import copy
 
 class Pokemon:
     def __init__(self, id ) -> None:
         self.__id = id
-        self.__pokemonData = self.loadData()
+        self.__pokemonData = None
         self.__name = None
         self.__type = None
-        self.__baseStats = None
         self.__stats = None
+        self.__growth = None
         self.__evolution = None
         self.__level = 1
         self.__xp = 0
@@ -18,6 +19,7 @@ class Pokemon:
         self.__imageFace = None
         self.__imageBack = None
         self.loadData()
+        self.__baseStats = copy.deepcopy(self.__pokemonData.get("stats"))
 
 
     def get_id(self):
@@ -47,36 +49,53 @@ class Pokemon:
     def get_imageBack(self):
         return self.__imageBack
     
+    def get_level(self):
+        return self.__level
+    
+    def set_level(self, level):
+        self.__level = level
+    
+    def get_growth(self):
+        return self.__growth
+    
+    def get_pokemonData(self):
+        return self.__pokemonData
+    
     # Charge les données du fichier pokemon.json
     def loadData(self):
         with open(r"data\pokemons\pokemons.json", "r") as fichier:
             donnees = json.load(fichier)
-
         # Rechercher les données du Pokémon avec l'ID correspondant
-        pokemon_data = next((pokemon for pokemon in donnees["pokemons"] if pokemon["id"] == self.__id), None)
-        
-        if pokemon_data:
-            self.__name = pokemon_data.get("name")
-            self.__type = pokemon_data.get("type")
-            self.__baseStats = pokemon_data.get("stats")
-            self.__evolution = pokemon_data.get("evolution")           
+        self.__pokemonData = None
+        for pokemon in donnees["pokemons"]:
+            if pokemon["id"] == self.__id:
+                self.__pokemonData = pokemon
+                break
+        if self.__pokemonData:
+            self.__name = self.__pokemonData.get("name")
+            self.__type = self.__pokemonData.get("type")
+            self.__baseStats = self.__pokemonData.get("stats")
+            if self.__stats is None:
+                self.__stats = self.__baseStats
+            self.__growth = self.__pokemonData.get("growth")
+            self.__evolution = self.__pokemonData.get("evolution")           
             self.__imageFace = pygame.image.load(f"images\\sprite_pokemon\\front\\{self.__id}.gif")
             self.__imageBack = pygame.image.load(f"images\\sprite_pokemon\\back\\{self.__id}.gif")
-        return pokemon_data
 
     # Test de l'affichage des stats
-    def afficherBaseInfos(self):
+    def afficherBaseStats(self):
+            
             print(f"#{self.__id} {self.__name} - Type: {self.__type}")
-            print("Stats:")
-            for stat, value in self.__stats.items():
-                print(f"  {stat.capitalize()}: {value}")
+            print("Stats de base:")
+            for baseStat, value in self.__baseStats.items():
+                print(f"  {baseStat.capitalize()}: {value}")
             print(f"  Level: {self.__level}")
             print(f"  XP: {self.__xp}")
             if self.__evolution:
                 print(f"Évolution: Niveau {self.__evolution['level']} vers {self.__evolution['to']}")
             print("\n")
 
-    def set_xp(self, AddXp): # xpGagnée à définir dans la class Combat
+    def set_xp(self, AddXp): # xp gagnée à définir dans la class Combat
         self.__xp += AddXp
 
         print(f"{self.__name} a gagné {AddXp} xp !") 
@@ -90,10 +109,12 @@ class Pokemon:
                         self.evolue()
 
     def get_abilities(self):
+
         self.__abilities = self.__pokemonData.get("abilities")
 
 
     def get_AbilitiesByLevel(self):
+
         abilities = []
         self.get_abilities()
         for ability in self.__abilities:
@@ -104,41 +125,74 @@ class Pokemon:
         return self.__abilities
 
     def level_up(self):
+
         self.__level += 1
         self.get_AbilitiesByLevel()
 
-        growth = self.__pokemonData.get("growth", {})
-        for stat, value in growth.items():
-            self.__stats[stat] = self.__baseStats[stat] + value * self.__level
-        print(self.afficherBaseInfos())
+        self.growUp()
+        print(self.afficherBaseStats())
+        print(self.afficherStats())
+
+    def updateStats(self):
+        self.__stats = self.__baseStats
+
+    def growUp(self):
+        if self.__stats is None:
+            self.updateStats()
+            
+        else:
+            growth = self.get_growth()
+            print(f"self.__growth: {self.__growth}")
+            growth = self.get_growth()
+            print(f"--self.__stats before growth: {self.__stats}")
+            for stat, value in growth.items():
+                self.__stats[stat] = self.__stats[stat] + value * (self.__level - 1)
+            print(f"--self.__stats after growth: {self.__stats}")
+
+    def afficherStats(self):
+        if self.__stats:
+
+            print(f"#{self.__id} {self.__name} - Type: {self.__type}")
+            print("Stats actuelles:/niveau:")
+            for stat, value in self.__stats.items():
+                print(f"  {stat.capitalize()}: {value}")
+            print(f"  Level: {self.__level}")
+            print(f"  XP: {self.__xp}")
+            print("\n")
 
     # Prend les valeurs de l'évolution id name stat etc...
     def evolue (self):
         evolutionName = self.get_name_by_id(self.__id + 1)
         print(f"{self.__name} a évolué en {evolutionName} !")
+
         self.__id = self.__evolution.get("to")
         self.loadData()
-        print(self.afficherBaseInfos())
+        self.updateStats()
+        print(self.afficherBaseStats())
 
     def get_name_by_id(self, idDonne):
         with open(r"data\pokemons\pokemons.json", "r") as fichier:
             donnees = json.load(fichier)
-
         # Rechercher les données du Pokémon avec l'ID correspondant
-        pokemonName = next((pokemon for pokemon in donnees["pokemons"] if pokemon["id"] == idDonne), None)
-        
+        pokemonName = None
+        for pokemon in donnees["pokemons"]:
+            if pokemon["id"] == idDonne:
+                pokemonName = pokemon["name"]
+                break
         if pokemonName:
-            evolutionName = pokemonName.get("name")
+            evolutionName = pokemonName
         return evolutionName
     
 
 # Test de la class
 
 starter = Pokemon (1)
-starter.afficherBaseInfos()
-starter.set_xp(250)
-starter.afficherBaseInfos()
-starter.set_xp (1600)
+print(starter.get_baseStats())
+starter.set_xp(100)
+print(starter.get_id())
+starter.set_xp(1500)
+print(starter.get_baseStats())
+starter.set_xp(1500)
 
 pygame.init()
 

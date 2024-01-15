739cdf76a236db85e01f6e766eef01feec76bb97
lyes-hamici <lyes.hamici@laplateforme.io>
2024-01-15 15:30:47 +0100

    Barre de vie qui fonctionne

--- src/class/Combat.py
+++ src/class/Combat.py
@@ -1,3 +1,4 @@
+from Button import *
 import pygame
 import sys
 from pygame import *
@@ -14,19 +15,23 @@ class Combat():
         self.__fond = ["images\\background\combat\sprite_combat_background01.png","images\\background\combat\sprite_combat_background02.png","images\\background\combat\sprite_combat_background03.png","images\\background\combat\sprite_combat_background04.png","images\\background\combat\sprite_combat_background05.png","images\\background\combat\sprite_combat_background06.png","images\\background\combat\sprite_combat_background07.png","images\\background\combat\sprite_combat_background08.png","images\\background\combat\sprite_combat_background09.png","images\\background\combat\sprite_combat_background10.png","images\\background\combat\sprite_combat_background11.png"]
         self.__choice = random.choice(self.__fond)
         self.__arene = pygame.image.load(self.__choice)
-        self.__size_image = (self.__WIDTH,self.__HEIGHT)
+        self.__size_image = (1000,500)
         self.__zone = pygame.transform.scale(self.__arene, self.__size_image)
         self.__liste_musique = ["musique\combat\Pokémon Black & White - Critical Health Music (HQ).mp3","musique\combat\Pokémon Diamond, Pearl & Platinum - Champion Cynthia Battle Music (HQ).mp3","musique\combat\Pokémon Omega Ruby & Alpha Sapphire - Primal Kyogre & Groudon Battle Music (HQ).mp3","musique\combat\Pokémon Omega Ruby & Alpha Sapphire - Zinnia Battle Music (HQ).mp3","musique\combat\Pokémon Scarlet & Violet - Champion Kieran Battle Music (HQ).mp3","musique\combat\Pokémon Scarlet & Violet - Champion Nemona Battle Music (HQ)(1).mp3","musique\combat\Pokémon Sun & Moon - Battle Legend Red & Blue Battle Music (HQ).mp3","musique\combat\Pokémon Sun & Moon - Rival Gladion Battle Music (HQ).mp3","musique\combat\Pokémon Sun & Moon - Team Skull Leader Guzma Battle Music (HQ).mp3","musique\combat\Pokémon HeartGold & SoulSilver - Champion & Red Battle Music (HQ).mp3"]
         self.__choice_musique = random.choice(self.__liste_musique)
         self.max_hp = 250
+        self.max_hp_adv = 250
+        self.multiplicateur_degat_adv = 1
         self.multiplicateur_degat = 1
         self.degats_effectuer = 0 * self.multiplicateur_degat
         self.hp = self.max_hp - self.degats_effectuer
+        self.hp_adv = self.max_hp_adv - self.degats_effectuer
         self.ratio = self.hp / self.max_hp
-        self.degats_subi_joueur = 0 #Créer une conditions si degat subis == hp max --> pokemon ko --> Victoire/defaites
-        self.degats_subi_adverssaire = 0 #Créer une conditions si degat subis == hp max --> pokemon ko --> Victoire/defaites
-        self.multiplicateur_joueur = int(self.max_hp) - self.degats_subi_joueur
-        self.multiplicateur_adverssaire = int(self.max_hp) - self.degats_subi_adverssaire
+        self.ratio_adv = self.hp_adv / self.max_hp_adv
+        self.degats_subi_joueur = 0 * self.multiplicateur_degat_adv #Créer une conditions si degat subis == hp max --> pokemon ko --> Victoire/defaites
+        self.degats_subi_adverssaire = 0 * self.multiplicateur_degat #Créer une conditions si degat subis == hp max --> pokemon ko --> Victoire/defaites
+        self.nouveau_hp = int(self.max_hp) - self.degats_subi_joueur
+        self.nouveau_hp_adv = int(self.max_hp) - self.degats_subi_adverssaire
         self.police = pygame.font.Font("font\Pokemon Classic.ttf", 10)
         self.__nom = self.police.render("Pikachu :", True, "black")
         self.__adversaire = self.police.render("Salamèche :", True, "black")
@@ -38,50 +43,114 @@ class Combat():
 
 
     def fight(self):
+        size_capa = (1000,200)
+        press = False
+        capa = pygame.image.load("images\\background\combat\panel.png")
+        capa = pygame.transform.scale(capa, size_capa)
         
         jouer = True
         pygame.display.set_caption("Fight")
         musique = pygame.mixer.music.load(self.__choice_musique)
         mixer.music.set_volume(0.2)
         mixer.music.play(-1)
-        print("Degat subi adv : ",self.degats_subi_adverssaire," , Degats effectuer : ",self.degats_effectuer)
-        self.update_degats()
         
+        self.__SCREEN.blit(capa, (0, 500))
+        self.__SCREEN.blit(self.__zone, (0, 0))
+        self.__SCREEN.blit(self.__nom, (30, 10))
+        self.__SCREEN.blit(self.__adversaire, (700, 10))
+        self.health_bar()
+
         while jouer:
-            self.__SCREEN.blit(self.__zone, (0, 0))
-            self.__SCREEN.blit(self.__nom, (30, 10))
-            self.__SCREEN.blit(self.__adversaire, (700, 10))
 
-            self.update_degats()
-            self.update_hp_adversse()
+            
+            MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-            self.health_bar()
-            print("Degat subi adv : ",self.degats_subi_adverssaire," , Degats effectuer : ",self.degats_effectuer)
-            print("Multiplicateur adverssaire : ",self.multiplicateur_adverssaire," , HP : ",self.hp)
-            pygame.display.update() 
+            ATT1 = Button(image=pygame.image.load("images\\button\images\\sprite_test3.png"), pos=(250, 550), 
+                                text_input="ATT1", font=self.police, base_color="#000000", hovering_color="White")
             
+            ATT2 = Button(image=pygame.image.load("images\\button\images\sprite_test3.png"), pos=(250, 650), 
+                                text_input="ATT2", font=self.police, base_color="#000000", hovering_color="White")
             
-            for events in event.get():
+            ATT3 = Button(image=pygame.image.load("images\\button\images\sprite_test3.png"), pos=(750, 550), 
+                                text_input="ATT3", font=self.police, base_color="#000000", hovering_color="White")
+            
+            ATT4 = Button(image=pygame.image.load("images\\button\images\sprite_test3.png"), pos=(750, 650), 
+                                text_input="ATT4", font=self.police, base_color="#000000", hovering_color="White")
+
+
+            for button in [ATT1,ATT2,ATT3,ATT4]:
+
+                button.changeColor(MENU_MOUSE_POS)
+
+                button.update(self.__SCREEN)
+
+            
+            for event in pygame.event.get():
+
+                if event.type == pygame.QUIT:
+
+                    pygame.quit()
+
+                    sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+
+                if ATT1.checkForInput(MENU_MOUSE_POS) and not press:
+                    self.hp_adv -= 30
+                    print(self.hp_adv)
+                    press = True
+                    self.ratio = self.hp / self.max_hp
+                    self.ratio_adv = self.hp_adv / self.max_hp_adv
+                    if self.hp_adv < 0:
+                        self.hp_adv = 0
+                        print(self.hp_adv)
+                    self.health_bar()
+                    
+
+
+            elif event.type == pygame.MOUSEBUTTONUP:
+                if  ATT1.checkForInput(MENU_MOUSE_POS):
+                    press = False
+
+
+
+
+                    
+
+
+
+
+                ##if ATT2.checkForInput(MENU_MOUSE_POS):
+
                 
+                    
 
-                if events.type == QUIT:
+                #if ATT3.checkForInput(MENU_MOUSE_POS):
 
-                    jouer=False
 
-                    quit()
+                #if ATT4.checkForInput(MENU_MOUSE_POS):
+
+                    
+
+
+                pygame.display.flip()
+
+            pygame.display.flip() 
+
+
 
 
 
     def health_bar(self):
-            #Joueur
-            pygame.draw.rect(self.__SCREEN, "white", (30, 30, 250, 10))
-            pygame.draw.rect(self.__SCREEN, "green", (30, 30,self.multiplicateur_joueur*self.ratio, 10))
-            pygame.draw.rect(self.__SCREEN, "black", (30, 30,250, 10),1)
+        # Dessiner la barre de vie du joueur
+        pygame.draw.rect(self.__SCREEN, (255,255,255), (30, 30, 250, 10))
+        pygame.draw.rect(self.__SCREEN, (0,255,0), (30, 30,250*self.ratio, 10))
+        pygame.draw.rect(self.__SCREEN, (0,0,0), (30, 30,250, 10),1)
 
-            #Pokémon
-            pygame.draw.rect(self.__SCREEN, "white", (700, 30, 250, 10))
-            pygame.draw.rect(self.__SCREEN, "green", (700, 30,self.multiplicateur_adverssaire*self.ratio, 10))
-            pygame.draw.rect(self.__SCREEN, "black", (700, 30,250, 10),1)
+        # Dessiner la barre de vie de l'adversaire
+        pygame.draw.rect(self.__SCREEN, (255,255,255), (700, 30, 250, 10))
+        pygame.draw.rect(self.__SCREEN, (0,255,0), (700, 30,250*self.ratio_adv, 10))
+        pygame.draw.rect(self.__SCREEN, (0,0,0), (700, 30,250, 10),1)
 
 
 
@@ -91,6 +160,20 @@ class Combat():
 
     def update_degats(self):
         self.degats_effectuer = 100 
+
+
+
+
+
+        '''import random #gere le taux de reussite des attaques (à adapter)
+
+        probabilite_reussite = 0.75
+        nombre_aleatoire = random.uniform(0, 1)
+
+        if nombre_aleatoire <= probabilite_reussite:
+            print("L'action a réussi!")
+        else:
+            print("L'action a échoué.")'''
                    
 
 
